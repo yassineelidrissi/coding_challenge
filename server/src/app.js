ea076ccd44407f5b68800aa4399d57a3f1f737f5
yassineelidrissi <yassineelidrissi02@gmail.com>
2024-01-01 19:12:54 +0100

    fix path && url

--- server/src/app.js
+++ server/src/app.js
@@ -37,11 +37,12 @@ app.use(hpp());
 app.use('/api', sectorRoutes);
 app.use('/api', userRoutes);
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
-})
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+});
+
 
 
 export default app;
