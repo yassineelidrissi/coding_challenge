ce5b0196c35976e8d025f105fffca085cac03bf3
yassineelidrissi <yassineelidrissi02@gmail.com>
2024-01-01 23:21:00 +0100

    remove rate limit && helmet

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,6 @@ import express from 'express';
 import cors from 'cors';
 import sectorRoutes from './routes/sector.route.js';
 import userRoutes from './routes/user.route.js';
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
 import mongoSanitize from 'express-mongo-sanitize';
 import xss from 'xss-clean';
 import hpp from 'hpp';
@@ -16,21 +14,7 @@ const __dirname = path.resolve();
 app.use(cors());
 app.options('*', cors());
 
-// app.use(helmet.contentSecurityPolicy({
-//     directives: {
-//       "script-src": ["'self'", "'nonce-YourRandomNonceValue'"],
-//     },
-// }));
-
-const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour !'
-});
-
-app.use('/api', limiter);
-
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json());
 
 app.use(mongoSanitize());
 
